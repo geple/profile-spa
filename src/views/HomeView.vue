ae27b843ea0f98f6bbfa370783d76dd467dc2041
George Lloyd <74584656+geple@users.noreply.github.com>
2023-03-31 15:17:52 +1100

    Made content margins dynamic

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,10 +8,10 @@
         <div class="about">
             <h2>Welcome to my website!</h2>
             <p><span>I am a mechanical engineer turned software engineer, searching for the place to begin my career. </span>
-            <span class="blast">Digital or mechanical, I enjoy building, breaking, and remaking. Human-centered, broadly 
+            <span>Digital or mechanical, I enjoy building, breaking, and remaking. Human-centered, broadly 
                 accessible tools are what I am excited by.</span>
             If you need a fresh and keen mind to work on any kind of software development project,
-                <a href="/contact" class="highlight">
+                <a href="/contact">
                     contact me!
                 </a></p>
         </div>
@@ -23,7 +23,4 @@
 </script>
 
 <style lang="scss" scoped>
-// div {
-//     display: inline-block;
-// }
 </style>
