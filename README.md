256be96d044ccecbba16314456826a56d0db081b
geple <gplloyd97@gmail.com>
2023-04-20 11:29:14 +1000

    Added automatic homepage screenshot generator and link in readme

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## The first iteration of my personal profile website, <a href="https://geple.page" target="_blank">geple.page</a> built with <a href="https://vuejs.org/" target="_blank">Vue 3</a> & <a href="https://vitejs.dev/" target="_blank">Vite</a> and deployed with <a href="https://www.netlify.com/" target="_blank">Netlify</a>.
 
 [![Netlify Status](https://api.netlify.com/api/v1/badges/018521de-5f16-454a-bdf4-e20916292768/deploy-status)](https://app.netlify.com/sites/soft-hummingbird-04c3b9/deploys)
+
+![Demo](https://raw.githubusercontent.com/geple/profile-spa/public/homepage.jpg)
+
---
+++ screenshot.js
@@ -0,0 +1,27 @@
+// Generates a screenshot of the site homepage for use in the Readme.md of the github repo
+
+import puppeteer from 'puppeteer';
+import { execSync } from 'child_process';
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    // Emulate dark theme
+    await page.emulateMediaFeatures('screen');
+    await page.emulateMediaFeatures([{ name: 'prefers-color-scheme', value: 'dark'}]);
+
+    await page.goto('https://geple.page/');
+
+    await page.screenshot({ path: 'public/homepage.jpg' });
+    await browser.close();
+})();
+
+// Push new homepage file to github
+execSync('git config user.name "geple');
+execSync('git config user.email "gplloyd97@gmail.com"');
+
+execSync('git add public/homepage.jpg');
+execSync('git commit -m "Auto update homepage screenshot"');
+
+execSync('git push origin master');
